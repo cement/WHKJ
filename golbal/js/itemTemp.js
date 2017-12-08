e454d9b182eb8be311ba6b182d3838c34cb8f6bb
YSH <32463636@qq.com>
2017-12-08 15:16:57 +0800

    replace banner pic

--- golbal/js/itemTemp.js
+++ golbal/js/itemTemp.js
@@ -1,4 +1,4 @@
-let inner =  `<span class="body-content-item-img">
+var inner =  `<span class="body-content-item-img">
                         <a href="#" terget="_blank"><img src="../golbal/img/logo.jpg" alt="测试" title="测试"></a>
                         </span>
                         <span class="body-content-item-describe">
@@ -7,15 +7,15 @@ let inner =  `<span class="body-content-item-img">
                             <p>this is a test item,and abc dce ,gspsdf sdgs sgsdgs dsggd sdgsdgv sgdsg sgd bsdgsgf sgdsgd sgsdgsd sgdsg d</p>
                             
                     </span>`;
-        let itembox = document.getElementById('body-content-main');
-        let content = document.createDocumentFragment();
+        var itembox = document.getElementById('body-content-main');
+        var content = document.createDocumentFragment();
         
-        let item   = document.createElement('div');
+        var item   = document.createElement('div');
         // item.addClass('body-content-item');
         item.setAttribute('class', 'body-content-item');
         item.innerHTML=inner;
 
-        for(let i = 0; i<100;i++){
+        for(var i = 0; i<100;i++){
              content.appendChild(item.cloneNode(true));
         }
 
